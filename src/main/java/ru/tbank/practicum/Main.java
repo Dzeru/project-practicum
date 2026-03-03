bf400c83fc28ea31d9d3082a8011d3a61a46c162
Diana <diana.mashkina.00@gmail.com>
2026-03-03 13:54:50 +0300

    Update Main.java

--- src/main/java/ru/tbank/practicum/Main.java
+++ src/main/java/ru/tbank/practicum/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Goodbye World!");
         System.out.println("And hello again!");
+        System.out.println("And again!");
     }
 }
